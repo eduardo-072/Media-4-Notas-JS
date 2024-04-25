068468fcc6155e15c458eaf8d3dff2f3aa79fcc4
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-04-25 20:23:06 -0300

    feito de novo

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
-let N1;
-let N2;
-let N3;
-let N4;
-
-alert(prompt("Digite um numero " + N1));
-alert(prompt("Digite um numero " + N2));
-alert(prompt("Digite um numero " + N3));
-alert(prompt("Digite um numero " + N4));
+let N1 = alert(prompt("Digite um numero " + N1));
+let N2 = alert(prompt("Digite um numero " + N2));
+let N3 = alert(prompt("Digite um numero " + N3));
+let N4 = alert(prompt("Digite um numero " + N4));
 
 let media = N1 + N2 + N3 + N4 / 4;
 
