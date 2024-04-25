73dbb3636392b19c94eacbf00d53a6b20a95ba34
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-04-25 20:43:54 -0300

    feito de novo

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
-let N1 = alert(prompt("Digite um numero " + N1));
-let N2 = alert(prompt("Digite um numero " + N2));
-let N3 = alert(prompt("Digite um numero " + N3));
-let N4 = alert(prompt("Digite um numero " + N4));
+let N1;
+let N2;
+let N3;
+let N4;
 
-let media = N1 + N2 + N3 + N4 / 4;
+prompt("Digite um numero " + N1);
+prompt("Digite um numero " + N2);
+prompt("Digite um numero " + N3);
+prompt("Digite um numero " + N4);
+
+let media = (N1 + N2 + N3 + N4) / 4;
 
 if (media >= 6)
 {
-    alert("VOCÊ FOI APROVADO");
+    alert("VOCÊ FOI APROVADO CABAÇO!!!");
 }
 else
 {
-    alert("VOCÊ FOI REPROVADO");
+    alert("VOCÊ FOI REPROVADO CABAÇO!!!");
 }
