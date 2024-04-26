792a01d3ef9e8aa9df3a6dd41a913b88804ea25e
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-04-25 22:34:50 -0300

    entendivel pela metade

--- script.js
+++ script.js
@@ -3,18 +3,19 @@ let N2;
 let N3;
 let N4;
 
-alert = parseFloat(prompt("Digite um numero " + N1));
-alert = parseFloat(prompt("Digite um numero " + N2));
-alert = parseFloat(prompt("Digite um numero " + N3));
-alert = parseFloat(prompt("Digite um numero " + N4));
-
+N1 = parseFloat(prompt("Digite um numero "  ));
+N2 = parseFloat(prompt("Digite um numero "  ));
+N3 = parseFloat(prompt("Digite um numero "  ));
+N4 = parseFloat(prompt("Digite um numero "  ));
 let media = (N1 + N2 + N3 + N4) / 4;
 
-if (media >= 6)
+alert("Sua média foi de " + media);
+
+if (media >= 6 && media <= 10) 
 {
-    alert("VOCÊ FOI APROVADO CABAÇO!!!");
-}
-else
+  alert(" VOCÊ FOI APROVADO CABAÇO!!!");
+} 
+else 
 {
-    alert("VOCÊ FOI REPROVADO CABAÇO!!!");
+  alert("VOCÊ FOI REPROVADO CABAÇO!!!");
 }
